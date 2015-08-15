5ef0ebbcda2e59137f62a0e7558c9dcb321f87d8
delta24 <viyat001@gmail.com>
2015-08-16 00:57:46 +0530

    Added default name for the extracted jar

--- framework/crawljax/start.sh
+++ framework/crawljax/start.sh
@@ -7,5 +7,5 @@ port=$1
 curr_dir=${PWD}
 
 #java -Dhttp.proxyHost=${host} -Dhttp.proxyPort=${proxy_port} -jar crawljax-web-3.6.jar -p ${port} -o ${output_folder}
-java -jar ${curr_dir}/framework/crawljax/crawljax_web.jar -p ${port}
+java -jar ${curr_dir}/framework/crawljax/crawljax-web-3.6.jar -p ${port}
 
